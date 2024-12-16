1cdccc7c0fb4cf49acd42d44eef76ca79dd36657
mwanjii <goerge.smmns@gmail.com>
2024-12-17 00:37:13 +0300

    Hue

--- src/components/global/sidebar/items.tsx
+++ src/components/global/sidebar/items.tsx
@@ -1,3 +1,35 @@
+// import { SIDEBAR_MENU } from '@/constants/menu'
+// import { cn } from '@/lib/utils'
+// import Link from 'next/link'
+// import React from 'react'
+
+// type Props = {
+//   page: string
+//   slug: string
+// }
+
+// const Items = ({ page, slug }: Props) => {
+//   return SIDEBAR_MENU.map((item) => (
+//     <Link
+//       key={item.id}
+//       href={`/dashboard/${slug}/${item.label === 'home' ? '/' : item.label}`}
+//       className={cn(
+//         'capitalize flex gap-x-2 rounded-full p-3',
+//         page === item.label && 'bg-[#0f0f0f]',
+//         page === slug && item.label === 'home'
+//           ? 'bg-[#0f0f0f]'
+//           : 'text-[#9B9CA0]'
+//       )}
+      
+//     >
+//       {item.icon}
+//       {item.label}
+//     </Link>
+//   ))
+// }
+
+// export default Items
+
 import { SIDEBAR_MENU } from '@/constants/menu'
 import { cn } from '@/lib/utils'
 import Link from 'next/link'
@@ -6,9 +38,10 @@ import React from 'react'
 type Props = {
   page: string
   slug: string
+  onLinkClick?: () => void // Added optional onLinkClick prop
 }
 
-const Items = ({ page, slug }: Props) => {
+const Items = ({ page, slug, onLinkClick }: Props) => {
   return SIDEBAR_MENU.map((item) => (
     <Link
       key={item.id}
@@ -20,7 +53,7 @@ const Items = ({ page, slug }: Props) => {
           ? 'bg-[#0f0f0f]'
           : 'text-[#9B9CA0]'
       )}
-      
+      onClick={onLinkClick} // Added onClick to trigger sidebar close
     >
       {item.icon}
       {item.label}
