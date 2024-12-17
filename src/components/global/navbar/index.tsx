e949920703cf06f7d58b4f1f72fc03393aeea09a
mwanjii <goerge.smmns@gmail.com>
2024-12-17 18:27:50 +0300

    Using SheetClose

--- src/components/global/navbar/index.tsx
+++ src/components/global/navbar/index.tsx
@@ -1,92 +1,95 @@
-'use client'
+ 'use client'
 
-import { PAGE_BREAD_CRUMBS } from '@/constants/pages'
-import { usePaths } from '@/hooks/user-nav'
-import { Menu } from 'lucide-react'
-import React, { useState } from 'react'
-import Sheet from '../sheet'
-import Items from '../sidebar/items'
-import { Separator } from '@/components/ui/separator'
-import ClerkAuthState from '../clerk-auth-state'
-import { HelpDuoToneWhite } from '@/icons'
-import { SubscriptionPlan } from '../subscription-plan'
-import UpgradeCard from '../sidebar/upgrade'
-import { LogoSmall } from '@/svgs/logo-small'
-import CreateAutomation from '../create-automation'
-import Search from './search'
-import { Notifications } from './notifications'
-import MainBreadCrumb from '../bread-crumbs/main-bread-crumb'
-import { SheetClose } from '@/components/ui/sheet' // Import SheetClose
+// import { PAGE_BREAD_CRUMBS } from '@/constants/pages'
+// import { usePaths } from '@/hooks/user-nav'
+// import { Menu } from 'lucide-react'
+// import React, { useState } from 'react'
+// import Sheet from '../sheet'
+// import Items from '../sidebar/items'
+// import { Separator } from '@/components/ui/separator'
+// import ClerkAuthState from '../clerk-auth-state'
+// import { HelpDuoToneWhite } from '@/icons'
+// import { SubscriptionPlan } from '../subscription-plan'
+// import UpgradeCard from '../sidebar/upgrade'
+// import { LogoSmall } from '@/svgs/logo-small'
+// import CreateAutomation from '../create-automation'
+// import Search from './search'
+// import { Notifications } from './notifications'
+// import MainBreadCrumb from '../bread-crumbs/main-bread-crumb'
+// import { SheetClose } from '@/components/ui/sheet' // Import SheetClose
+// import Link from 'next/link'
 
-type Props = {
-  slug: string
-}
+// type Props = {
+//   slug: string
+// }
 
-const Navbar = ({ slug }: Props) => {
-  const { page } = usePaths()
-  const currentPage = PAGE_BREAD_CRUMBS.includes(page) || page == slug
+// const Navbar = ({ slug }: Props) => {
+//   const { page } = usePaths()
+//   const currentPage = PAGE_BREAD_CRUMBS.includes(page) || page == slug
   
   
-  return (
-    currentPage && (
-      <div className="flex flex-col">
-        <div className="flex gap-x-3 lg:gap-x-5 justify-end">
-          <span className="lg:hidden flex items-center flex-1 gap-x-2">
-            <Sheet
-              trigger={<Menu />}
-              className="lg:hidden"
-              side="left"
-            >
-              <div className="flex flex-col gap-y-5 w-full h-full p-3 bg-[#0e0e0e] bg-opacity-90 bg-clip-padding backdrop-filter backdrop--blur__safari backdrop-blur-3xl">
-                <div className="flex gap-x-2 items-center p-5 justify-center">
-                  <LogoSmall />
-                </div>
-                <div className="flex-1 h-120 overflow-y-auto py-2">
-                <SheetClose asChild>
-                  <Items
-                    page={page}
-                    slug={slug}                    
-                  />
-                  </SheetClose>                  
-                </div>
-                <div className="px-16">
-                  <Separator
-                    orientation="horizontal"
-                    className="bg-[#333336]"
-                  />
-                </div>
-                <div className="px-3 flex flex-col gap-y-5">
-                  <div className="flex gap-x-2">
-                    <ClerkAuthState />
-                    <p className="text-[#9B9CA0]">Profile</p>
-                  </div>
-                  <div className="flex gap-x-3">
-                    <HelpDuoToneWhite />
-                    <p className="text-[#9B9CA0]">Help</p>
-                  </div>
-                </div>
-                <SubscriptionPlan type="FREE">
-                  <div className="flex-1 flex flex-col justify-end">
-                    <UpgradeCard />
-                  </div>
-                </SubscriptionPlan>
-              </div>
-            </Sheet>
-          </span>
-          <Search />
-          <CreateAutomation />
-          <Notifications />
-        </div>
-        <MainBreadCrumb
-          page={page === slug ? 'Home' : page}
-          slug={slug}
-        />
-      </div>
-    )
-  )
-}
+//   return (
+//     currentPage && (
+//       <div className="flex flex-col">
+//         <div className="flex gap-x-3 lg:gap-x-5 justify-end">
+//           <span className="lg:hidden flex items-center flex-1 gap-x-2">
+//             <Sheet
+//               trigger={<Menu />}
+//               className="lg:hidden"
+//               side="left"
+//             >
+//               <div className="flex flex-col gap-y-5 w-full h-full p-3 bg-[#0e0e0e] bg-opacity-90 bg-clip-padding backdrop-filter backdrop--blur__safari backdrop-blur-3xl">
+//                 <div className="flex gap-x-2 items-center p-5 justify-center">
+//                   <LogoSmall />
+//                 </div>
+//                 <div className="flex-1 h-120 overflow-y-auto py-2">
+//                 <SheetClose asChild>
+                 
+//                   <Items
+//                     page={page}
+//                     slug={slug}                    
+//                   />
+                  
+//                   </SheetClose>                  
+//                 </div>
+//                 <div className="px-16">
+//                   <Separator
+//                     orientation="horizontal"
+//                     className="bg-[#333336]"
+//                   />
+//                 </div>
+//                 <div className="px-3 flex flex-col gap-y-5">
+//                   <div className="flex gap-x-2">
+//                     <ClerkAuthState />
+//                     <p className="text-[#9B9CA0]">Profile</p>
+//                   </div>
+//                   <div className="flex gap-x-3">
+//                     <HelpDuoToneWhite />
+//                     <p className="text-[#9B9CA0]">Help</p>
+//                   </div>
+//                 </div>
+//                 <SubscriptionPlan type="FREE">
+//                   <div className="flex-1 flex flex-col justify-end">
+//                     <UpgradeCard />
+//                   </div>
+//                 </SubscriptionPlan>
+//               </div>
+//             </Sheet>
+//           </span>
+//           <Search />
+//           <CreateAutomation />
+//           <Notifications />
+//         </div>
+//         <MainBreadCrumb
+//           page={page === slug ? 'Home' : page}
+//           slug={slug}
+//         />
+//       </div>
+//     )
+//   )
+// }
 
-export default Navbar
+// export default Navbar
 
 // 'use client'
 
@@ -193,3 +196,107 @@ export default Navbar
 // }
 
 // export default Navbar
+
+
+
+
+import { PAGE_BREAD_CRUMBS } from '@/constants/pages'
+import { SIDEBAR_MENU } from '@/constants/menu'
+import { usePaths } from '@/hooks/user-nav'
+import { Menu } from 'lucide-react'
+import React from 'react'
+import Sheet from '../sheet'
+import { Separator } from '@/components/ui/separator'
+import ClerkAuthState from '../clerk-auth-state'
+import { HelpDuoToneWhite } from '@/icons'
+import { SubscriptionPlan } from '../subscription-plan'
+import UpgradeCard from '../sidebar/upgrade'
+import { LogoSmall } from '@/svgs/logo-small'
+import CreateAutomation from '../create-automation'
+import Search from './search'
+import { Notifications } from './notifications'
+import MainBreadCrumb from '../bread-crumbs/main-bread-crumb'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { SheetClose } from '@/components/ui/sheet'
+
+type Props = {
+  slug: string
+}
+
+const Navbar = ({ slug }: Props) => {
+  const { page } = usePaths()
+  const currentPage = PAGE_BREAD_CRUMBS.includes(page) || page == slug
+
+  return (
+    currentPage && (
+      <div className="flex flex-col">
+        <div className="flex gap-x-3 lg:gap-x-5 justify-end">
+          <span className="lg:hidden flex items-center flex-1 gap-x-2">
+            <Sheet
+              trigger={<Menu />}
+              className="lg:hidden"
+              side="left"
+            >
+              <div className="flex flex-col gap-y-5 w-full h-full p-3 bg-[#0e0e0e] bg-opacity-90 bg-clip-padding backdrop-filter backdrop--blur__safari backdrop-blur-3xl">
+                <div className="flex gap-x-2 items-center p-5 justify-center">
+                  <LogoSmall />
+                </div>
+                <div className="flex-1 h-120 overflow-y-auto py-2">
+                  {/* Inline Items Component */}
+                  {SIDEBAR_MENU.map((item) => (
+                    <SheetClose asChild key={item.id}>
+                      <Link
+                        href={`/dashboard/${slug}/${item.label === 'home' ? '/' : item.label}`}
+                        className={cn(
+                          'capitalize flex gap-x-2 rounded-full p-3',
+                          page === item.label && 'bg-[#0f0f0f]',
+                          page === slug && item.label === 'home'
+                            ? 'bg-[#0f0f0f]'
+                            : 'text-[#9B9CA0]'
+                        )}
+                      >
+                        {item.icon}
+                        {item.label}
+                      </Link>
+                    </SheetClose>
+                  ))}
+                </div>
+                <div className="px-16">
+                  <Separator
+                    orientation="horizontal"
+                    className="bg-[#333336]"
+                  />
+                </div>
+                <div className="px-3 flex flex-col gap-y-5">
+                  <div className="flex gap-x-2">
+                    <ClerkAuthState />
+                    <p className="text-[#9B9CA0]">Profile</p>
+                  </div>
+                  <div className="flex gap-x-3">
+                    <HelpDuoToneWhite />
+                    <p className="text-[#9B9CA0]">Help</p>
+                  </div>
+                </div>
+                <SubscriptionPlan type="FREE">
+                  <div className="flex-1 flex flex-col justify-end">
+                    <UpgradeCard />
+                  </div>
+                </SubscriptionPlan>
+              </div>
+            </Sheet>
+          </span>
+          <Search />
+          <CreateAutomation />
+          <Notifications />
+        </div>
+        <MainBreadCrumb
+          page={page === slug ? 'Home' : page}
+          slug={slug}
+        />
+      </div>
+    )
+  )
+}
+
+export default Navbar
