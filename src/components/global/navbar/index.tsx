31183df198fb5e290e2ec42b18011764066d996e
mwanjii <goerge.smmns@gmail.com>
2024-12-17 20:34:50 +0300

    Using SheetClose

--- src/components/global/navbar/index.tsx
+++ src/components/global/navbar/index.tsx
@@ -238,7 +238,10 @@ const Navbar = ({ slug }: Props) => {
               className="lg:hidden"
               side="left"
             >
-              <div className="flex flex-col gap-y-5 w-full border-[2px] radial rounded-3xl overflow-hidden border-[#545454] h-full p-3 bg-[#0e0e0e] bg-opacity-90 bg-clip-padding backdrop-filter backdrop--blur__safari backdrop-blur-3xl">
+             
+        
+        
+              <div className="flex flex-col gap-y-5 w-full  bottom-2 top-2 m-3 border-[2px] radial rounded-3xl overflow-hidden border-[#545454] h-full p-3 bg-[#0e0e0e] bg-opacity-90 bg-clip-padding backdrop-filter backdrop--blur__safari backdrop-blur-3xl">
                 <div className="flex gap-x-2 items-center p-5 justify-center">
                   <LogoSmall />
                 </div>
