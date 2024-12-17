a807eecddd6005be1e51912bdb0d27d86b39aeba
mwanjii <goerge.smmns@gmail.com>
2024-12-17 17:54:03 +0300

    Using SheetClose

--- src/components/global/sidebar/items.tsx
+++ src/components/global/sidebar/items.tsx
@@ -30,10 +30,43 @@
 
 // export default Items
 
+// import { SIDEBAR_MENU } from '@/constants/menu'
+// import { cn } from '@/lib/utils'
+// import Link from 'next/link'
+// import React from 'react'
+
+// type Props = {
+//   page: string
+//   slug: string
+// }
+
+// const Items = ({ page, slug }: Props) => {
+//   return SIDEBAR_MENU.map((item) => (
+//     <Link
+//       key={item.id}
+//       href={`/dashboard/${slug}/${item.label === 'home' ? '/' : item.label}`}
+//       className={cn(
+//         'capitalize flex gap-x-2 rounded-full p-3',
+//         page === item.label && 'bg-[#0f0f0f]',
+//         page === slug && item.label === 'home'
+//           ? 'bg-[#0f0f0f]'
+//           : 'text-[#9B9CA0]'
+//       )}
+      
+//     >
+//       {item.icon}
+//       {item.label}
+//     </Link>
+//   ))
+// }
+
+// export default Items
+
 import { SIDEBAR_MENU } from '@/constants/menu'
 import { cn } from '@/lib/utils'
 import Link from 'next/link'
 import React from 'react'
+import { SheetClose } from '@/components/ui/sheet'
 
 type Props = {
   page: string
@@ -41,24 +74,27 @@ type Props = {
 }
 
 const Items = ({ page, slug }: Props) => {
-  return SIDEBAR_MENU.map((item) => (
-    <Link
-      key={item.id}
-      href={`/dashboard/${slug}/${item.label === 'home' ? '/' : item.label}`}
-      className={cn(
-        'capitalize flex gap-x-2 rounded-full p-3',
-        page === item.label && 'bg-[#0f0f0f]',
-        page === slug && item.label === 'home'
-          ? 'bg-[#0f0f0f]'
-          : 'text-[#9B9CA0]'
-      )}
-      
-    >
-      {item.icon}
-      {item.label}
-    </Link>
-  ))
+  return (
+    <>
+      {SIDEBAR_MENU.map((item) => (
+        <SheetClose asChild key={item.id}>
+          <Link
+            href={`/dashboard/${slug}/${item.label === 'home' ? '/' : item.label}`}
+            className={cn(
+              'capitalize flex gap-x-2 rounded-full p-3',
+              page === item.label && 'bg-[#0f0f0f]',
+              page === slug && item.label === 'home'
+                ? 'bg-[#0f0f0f]'
+                : 'text-[#9B9CA0]'
+            )}
+          >
+            {item.icon}
+            {item.label}
+          </Link>
+        </SheetClose>
+      ))}
+    </>
+  )
 }
 
 export default Items
-
