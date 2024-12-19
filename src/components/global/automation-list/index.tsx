feb24c7304e576362a3e3efe178184cad9d4c191
mwanjii <goerge.smmns@gmail.com>
2024-12-19 23:02:36 +0300

    Adjusting Delete Automation

--- src/components/global/automation-list/index.tsx
+++ src/components/global/automation-list/index.tsx
@@ -76,8 +76,9 @@ const AutomationList = ({ id }: Props) => {
           <div className="flex flex-col flex-1 items-start">
             <h2 className="text-xl font-semibold">{automation.name}</h2>
             <p className="text-[#9B9CA0] text-sm font-light mb-2">
-              This automation is for comments
+              {automation.type === 'COMMENT' ? 'This automation is for comments' : 'This automation is for posts'}
             </p>
+
             {automation.keywords.length > 0 ? (
               <div className="flex gap-x-2 flex-wrap mt-3">
                 {
