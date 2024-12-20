368a937087870b49cf955c6e50b4b59bd09baf9e
mwanjii <goerge.smmns@gmail.com>
2024-12-20 12:12:04 +0300

    Automations

--- src/components/global/automation-list/index.tsx
+++ src/components/global/automation-list/index.tsx
@@ -79,7 +79,7 @@ const AutomationList = ({ id }: Props) => {
           key={automation.id}
         >
           <div className="flex flex-col flex-1 items-start">
-            <h2 className="text-xl font-semibold">{automation.name}</h2>
+            <h2 className="text-xl font-semibold">{automation.id}</h2>
             <p className="text-[#9B9CA0] text-sm font-light mb-2">
               {automation.type === 'DM' ? 'This automation is for posts' : 'This automation is for comments'}
               
