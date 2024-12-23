1f75d044b6f0202a47554f28fa1286e6bc307e8a
mwanjii <goerge.smmns@gmail.com>
2024-12-23 20:16:14 +0300

    Home Page

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -77,7 +77,7 @@ export default function Home() {
         <FAQ />
         <CTA />
         <Footer />
-        {/* <FloatingNotification /> */}
+        <FloatingNotification />
       </main>
     </div>
   );
