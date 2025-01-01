dc67d9972f3d41ad780884c0f2b7393880316ac5
mwanjii <goerge.smmns@gmail.com>
2025-01-01 16:49:04 +0300

    Comments

--- src/components/global/automation-list/index.tsx
+++ src/components/global/automation-list/index.tsx
@@ -20,8 +20,6 @@ type Listener = {
   automationId: string;
   prompt: string;
   commentReply: string | null;
-  lastComment:string;
-  lastDm:string;
   dmCount: number;
   commentCount: number;
 };
