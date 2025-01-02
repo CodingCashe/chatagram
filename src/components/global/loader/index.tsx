b815712d182b3ad20c50651c231343143712cc64
mwanjii <goerge.smmns@gmail.com>
2025-01-02 23:07:06 +0300

    Comments

--- src/components/global/loader/index.tsx
+++ src/components/global/loader/index.tsx
@@ -1,29 +1,5 @@
-// import { cn } from '@/lib/utils'
-// import React from 'react'
-// import { Spinner } from './spinner'
-
-// type Props = {
-//   state: boolean
-//   className?: string
-//   children: React.ReactNode
-//   color?: string
-// }
-
-// const Loader = ({ children, state, className, color }: Props) => {
-//   return state ? (
-//     <div className={cn(className)}>
-//       <Spinner color={color} />
-//     </div>
-//   ) : (
-//     children
-//   )
-// }
-
-// export default Loader
-
-
-import React from 'react'
 import { cn } from '@/lib/utils'
+import React from 'react'
 import { Spinner } from './spinner'
 
 type Props = {
@@ -31,14 +7,12 @@ type Props = {
   className?: string
   children: React.ReactNode
   color?: string
-  size?: string
-  speed?: string
 }
 
-const Loader = ({ children, state, className, color, size = 'w-12 h-12', speed = 'animate-spin' }: Props) => {
+const Loader = ({ children, state, className, color }: Props) => {
   return state ? (
-    <div className={cn('fixed inset-0 flex items-center justify-center bg-black bg-opacity-40 z-50', className)}>
-      <Spinner color={color} size={size} speed={speed} />
+    <div className={cn(className)}>
+      <Spinner color={color} />
     </div>
   ) : (
     children
@@ -46,4 +20,30 @@ const Loader = ({ children, state, className, color, size = 'w-12 h-12', speed =
 }
 
 export default Loader
+
+
+// import React from 'react'
+// import { cn } from '@/lib/utils'
+// import { Spinner } from './spinner'
+
+// type Props = {
+//   state: boolean
+//   className?: string
+//   children: React.ReactNode
+//   color?: string
+//   size?: string
+//   speed?: string
+// }
+
+// const Loader = ({ children, state, className, color, size = 'w-12 h-12', speed = 'animate-spin' }: Props) => {
+//   return state ? (
+//     <div className={cn('fixed inset-0 flex items-center justify-center bg-black bg-opacity-40 z-50', className)}>
+//       <Spinner color={color} size={size} speed={speed} />
+//     </div>
+//   ) : (
+//     children
+//   )
+// }
+
+// export default Loader
 
