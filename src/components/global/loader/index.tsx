b5be0672155dce9b8d288fd989981bc1cb3f063d
mwanjii <goerge.smmns@gmail.com>
2025-01-02 13:48:41 +0300

    Comments

--- src/components/global/loader/index.tsx
+++ src/components/global/loader/index.tsx
@@ -1,5 +1,29 @@
-import { cn } from '@/lib/utils'
+// import { cn } from '@/lib/utils'
+// import React from 'react'
+// import { Spinner } from './spinner'
+
+// type Props = {
+//   state: boolean
+//   className?: string
+//   children: React.ReactNode
+//   color?: string
+// }
+
+// const Loader = ({ children, state, className, color }: Props) => {
+//   return state ? (
+//     <div className={cn(className)}>
+//       <Spinner color={color} />
+//     </div>
+//   ) : (
+//     children
+//   )
+// }
+
+// export default Loader
+
+
 import React from 'react'
+import { cn } from '@/lib/utils'
 import { Spinner } from './spinner'
 
 type Props = {
@@ -7,12 +31,14 @@ type Props = {
   className?: string
   children: React.ReactNode
   color?: string
+  size?: string
+  speed?: string
 }
 
-const Loader = ({ children, state, className, color }: Props) => {
+const Loader = ({ children, state, className, color, size = 'w-12 h-12', speed = 'animate-spin' }: Props) => {
   return state ? (
-    <div className={cn(className)}>
-      <Spinner color={color} />
+    <div className={cn('fixed inset-0 flex items-center justify-center bg-black bg-opacity-40 z-50', className)}>
+      <Spinner color={color} size={size} speed={speed} />
     </div>
   ) : (
     children
