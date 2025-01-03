5f2a9d13b26596811dcedd7eb50ba8b4f0db602c
mwanjii <goerge.smmns@gmail.com>
2025-01-03 10:33:08 +0300

    Comments

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -25,7 +25,7 @@ type Props = {}
 const Page = async (props: Props) => {
   const user = await onBoardUser()
   if (user.status === 200 || user.status === 201) {
-    return redirect(`dashboard/${user.data?.firstname} ${user.data?.lastname}`)
+    return redirect(`dashboard/${user.data?.firstname}-${user.data?.lastname}`)
     // return redirect(`dashboard/${user.data?.firstname}`)
   }
 
@@ -33,3 +33,4 @@ const Page = async (props: Props) => {
 }
 
 export default Page
+
