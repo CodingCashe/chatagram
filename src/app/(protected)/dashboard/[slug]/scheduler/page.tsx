a4a00de5b261705cbc00b4e772076457d3f8d9a1
mwanjii <goerge.smmns@gmail.com>
2025-01-03 16:08:55 +0300

    Comments

--- src/app/(protected)/dashboard/[slug]/scheduler/page.tsx
+++ src/app/(protected)/dashboard/[slug]/scheduler/page.tsx
@@ -2,8 +2,7 @@ import InstagramPostScheduler from './instascheduler/InstagramPostScheduler'
 
 export default function Home() {
   return (
-    <main className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-8">Instagram Post Scheduler</h1>
+    <main className="container mx-auto p-4">      
       <InstagramPostScheduler />
     </main>
   )
