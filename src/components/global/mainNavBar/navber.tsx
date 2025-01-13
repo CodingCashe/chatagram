3a3ead99bd205401324b7f2927d27ae0dd59a5cd
mwanjii <goerge.smmns@gmail.com>
2025-01-13 16:54:51 +0300

    Mascott

--- src/components/global/mainNavBar/navber.tsx
+++ src/components/global/mainNavBar/navber.tsx
@@ -16,9 +16,8 @@ type FixedNavbarProps = {
 
 const FixedNavbar: React.FC<FixedNavbarProps> = ({ slug, fullPageName, displayName, isUUID }) => {
   return (
-    <div className="fixed w-full top-0 right-0 z-50">
-      <div className="flex gap-x-3 lg:gap-x-5 items-center justify-between px-4 py-2">
-        <MenuButton />
+    <div className="fixed top-0 right-0 z-50">
+      <div className="flex gap-x-3 lg:gap-x-5 items-center justify-between px-4 py-2">       
         <Search />
         <CreateAutomation />
         <Notifications />        
