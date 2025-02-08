caf04647ad56ef59a289bd32bbd1850fe9a80f70
mwanjii <goerge.smmns@gmail.com>
2025-02-08 13:30:53 +0300

    Vf

--- src/components/global/integrate/index.tsx
+++ src/components/global/integrate/index.tsx
@@ -1,36 +1,62 @@
+// "use client"
+
+// import { Button } from "@/components/ui/button"
+// import { useMemo } from "react"
+// import Loader from "../loader"
+// import { useCreateAutomation } from "@/hooks/use-automations"
+// import { v4 } from "uuid"
+// import { Instagram } from "lucide-react"
+
+// type Props = {}
+
+// const IntegrateAccount = (props: Props) => {
+//   const mutationId = useMemo(() => v4(), [])
+
+//   console.log(mutationId)
+//   const { isPending, mutate } = useCreateAutomation(mutationId)
+
+//   return (
+//     <Button
+//       className="hidden lg:flex items-center px-10 py-6 bg-gradient-to-br hover:bg-gradient-to-tl hover:shadow-lg text-white rounded-full from-[#1A1E2D] to-[#2C3E50] font-medium transition-all duration-300 ease-in-out"
+//       onClick={() =>
+//         mutate({
+//           name: "Untitled",
+//           id: mutationId,
+//           createdAt: new Date(),
+//           keywords: [],
+//         })
+//       }
+//     >
+//       <Loader state={isPending}>
+//         <Instagram className="mr-2 h-5 w-5 text-[#E1306C]" />
+//         <span>Integrate Account</span>
+//       </Loader>
+//     </Button>
+//   )
+// }
+
+// export default IntegrateAccount
+
 "use client"
 
 import { Button } from "@/components/ui/button"
-import { useMemo } from "react"
-import Loader from "../loader"
-import { useCreateAutomation } from "@/hooks/use-automations"
-import { v4 } from "uuid"
 import { Instagram } from "lucide-react"
+import { useRouter } from "next/navigation"
 
-type Props = {}
+const IntegrateAccount = () => {
+  const router = useRouter()
 
-const IntegrateAccount = (props: Props) => {
-  const mutationId = useMemo(() => v4(), [])
-
-  console.log(mutationId)
-  const { isPending, mutate } = useCreateAutomation(mutationId)
+  const handleIntegrate = () => {
+    router.push("/integrations")
+  }
 
   return (
     <Button
       className="hidden lg:flex items-center px-10 py-6 bg-gradient-to-br hover:bg-gradient-to-tl hover:shadow-lg text-white rounded-full from-[#1A1E2D] to-[#2C3E50] font-medium transition-all duration-300 ease-in-out"
-      onClick={() =>
-        mutate({
-          name: "Untitled",
-          id: mutationId,
-          createdAt: new Date(),
-          keywords: [],
-        })
-      }
+      onClick={handleIntegrate}
     >
-      <Loader state={isPending}>
-        <Instagram className="mr-2 h-5 w-5 text-[#E1306C]" />
-        <span>Integrate Account</span>
-      </Loader>
+      <Instagram className="mr-2 h-5 w-5 text-[#E1306C]" />
+      <span>Integrate Account</span>
     </Button>
   )
 }
