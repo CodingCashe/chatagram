82f36db2d4a928044d71f4b34a021000b4901879
mwanjii <goerge.smmns@gmail.com>
2025-02-16 20:46:46 +0300

    Api

--- src/app/(protected)/dashboard/[slug]/_components/instagram/PostScheduleForm.tsx
+++ src/app/(protected)/dashboard/[slug]/_components/instagram/PostScheduleForm.tsx
@@ -112,6 +112,10 @@ const mediaOptions = [
     url: "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1080&q=80",
   },
   {
+    name: "Fancy",
+    url: "https://picsum.photos/1080/1080?random=1",
+  },
+  {
     name: "Dog",
     url: "https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1080&q=80",
   },
