6b46c7e6d0cccb3bdb0e96d65bb78ab1a64ad37c
mwanjii <goerge.smmns@gmail.com>
2025-03-19 18:43:42 +0300

    no ads

--- prisma/migrations/20250319104601_worked/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `instagramHandle` on table `Business` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Business" ALTER COLUMN "instagramHandle" SET NOT NULL;
