9ba5348304efc7fbc68dd8bfb6a6df7ca59c961b
mwanjii <goerge.smmns@gmail.com>
2025-03-23 12:35:31 +0300

    Chats

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,16 +1,45 @@
-import { onBoardUser } from '@/actions/user'
-import { redirect } from 'next/navigation'
-import React from 'react'
+// import { onBoardUser } from '@/actions/user'
+// import { redirect } from 'next/navigation'
+// import React from 'react'
+
+// type Props = {}
+
+// const Page = async (props: Props) => {
+//   const user = await onBoardUser()
+//   if (user.status === 200 || user.status === 201) {
+//     return redirect(`dashboard/${user.data?.firstname}-${user.data?.lastname}`)
+//   }
+
+//   return redirect('/sign-in')
+// }
+
+// export default Page
+
+import { onBoardUser } from "@/actions/user"
+import { checkIsAdmin } from "../admin/actions"
+import { redirect } from "next/navigation"
 
 type Props = {}
 
 const Page = async (props: Props) => {
   const user = await onBoardUser()
+
+  // If user is authenticated
   if (user.status === 200 || user.status === 201) {
+    // Check if user is an admin
+    const isAdmin = await checkIsAdmin()
+
+    if (isAdmin) {
+      // Redirect admin users to the admin dashboard
+      return redirect("/admin")
+    }
+
+    // Redirect regular users to their dashboard
     return redirect(`dashboard/${user.data?.firstname}-${user.data?.lastname}`)
   }
 
-  return redirect('/sign-in')
+  // Redirect unauthenticated users to sign-in
+  return redirect("/sign-in")
 }
 
 export default Page
