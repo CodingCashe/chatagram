1ac5da314429b7467399505eb10e2b14719b1b74
mwanjii <goerge.smmns@gmail.com>
2025-04-01 22:24:43 +0300

    Whatsapp

---
+++ src/types/whatsapp.ts
@@ -0,0 +1,13 @@
+export interface WhatsAppBusinessAccount {
+    id: string
+    name: string
+    // Add other properties as needed
+  }
+  
+  export interface WhatsAppPhoneNumber {
+    id: string
+    display_phone_number: string
+    // Add other properties as needed
+  }
+  
+  
