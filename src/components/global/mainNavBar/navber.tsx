3af87ca0e237c015836f92f3f111904159722549
mwanjii <goerge.smmns@gmail.com>
2025-04-03 14:39:32 +0300

    Domain

--- src/components/global/mainNavBar/navber.tsx
+++ src/components/global/mainNavBar/navber.tsx
@@ -21,12 +21,12 @@ const FixedNavbar: React.FC<FixedNavbarProps> = ({ slug, fullPageName, displayNa
   return (
     <div className="fixed mb-100 top-0 right-0 z-50">
       <div className="flex gap-x-2 lg:gap-x-3 items-center justify-between px-2 py-2">  
-        <IntegrateAccount />           
-        <SchedulePost />  
-        {/* <AutomationDashboardHeader/>          */}
-        <Search />
+        {/* <IntegrateAccount />           
+        <SchedulePost />   */}
+        <AutomationDashboardHeader/>         
+        {/* <Search />
         <CreateAutomation />
-        <Notifications />        
+        <Notifications />         */}
       </div>
     </div>
   )
